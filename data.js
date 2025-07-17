e0949cae2242ecb9a6bb73e41f799cecb56227cb
Akuma_Yu <manjusaka1110@gmail.com>
2025-07-17 10:21:30 +0800

    javascript語言學習

---
+++ data.js
@@ -0,0 +1,72 @@
+data = [
+  {
+    "id": 1,
+    "weaponName": "龍戰九天",
+    "holder": "素還真",
+    "function": "素還真的主要武器，融合龍形力量，具備強大攻擊力及防禦力。",
+    "image": "https://pic.piliapp.com/weapon/202203/1646044207762_20.png"
+  },
+  {
+    "id": 2,
+    "weaponName": "天魔刀",
+    "holder": "誅天",
+    "function": "誅天的武器，能吸取敵人精氣並釋放強烈殺氣。",
+    "image": "https://pic.piliapp.com/weapon/202203/1646044207762_02.png"
+  },
+  {
+    "id": 3,
+    "weaponName": "混元重劍",
+    "holder": "葉小釵",
+    "function": "重劍，融合陰陽之力，能淨化邪惡。",
+    "image": "https://pic.piliapp.com/weapon/202203/1646044207762_11.png"
+  },
+  {
+    "id": 4,
+    "weaponName": "御皇",
+    "holder": "疏樓龍宿",
+    "function": "象徵皇權的武器，蘊含強大氣勢與威嚴。",
+    "image": "https://pic.piliapp.com/weapon/202203/1646044207762_17.png"
+  },
+  {
+    "id": 5,
+    "weaponName": "飛霜",
+    "holder": "一頁書",
+    "function": "擅長快速攻擊的雙刀，冰霜氣息圍繞，能使敵人行動受阻。",
+    "image": "https://pic.piliapp.com/weapon/202203/1646044207762_08.png"
+  },
+  {
+    "id": 6,
+    "weaponName": "混沌扇",
+    "holder": "羅喉",
+    "function": "強力法器，掌控混沌與魔能，擁有強大的法術攻擊力。",
+    "image": "https://pic.piliapp.com/weapon/202203/1646044207762_22.png"
+  },
+  {
+    "id": 7,
+    "weaponName": "幽冥鏡",
+    "holder": "羅喉",
+    "function": "能反射攻擊並迷惑敵人，是羅喉的重要暗器。",
+    "image": "https://pic.piliapp.com/weapon/202203/1646044207762_23.png"
+  },
+  {
+    "id": 8,
+    "weaponName": "霹靂火",
+    "holder": "葉小釵",
+    "function": "以火焰為武器核心，能爆發強烈火焰攻擊。",
+    "image": "https://pic.piliapp.com/weapon/202203/1646044207762_07.png"
+  },
+  {
+    "id": 9,
+    "weaponName": "劍神",
+    "holder": "素還真",
+    "function": "素還真的傳說之劍，靈動迅速且威力無比。",
+    "image": "https://pic.piliapp.com/weapon/202203/1646044207762_05.png"
+  },
+  {
+    "id": 10,
+    "weaponName": "霜華",
+    "holder": "一頁書",
+    "function": "冰霜之劍，能冰封敵人並造成致命一擊。",
+    "image": "https://pic.piliapp.com/weapon/202203/1646044207762_10.png"
+  }
+]
